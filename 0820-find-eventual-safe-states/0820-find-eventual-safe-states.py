7dc16b00d72ec966ce7e2d14afe7b70d990390e9
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-01-24 10:47:11 +0300

    Time: 35 ms (74.63%), Space: 23.7 MB (39.31%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -0,0 +1,23 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        l = len(graph)
+        safe = {}
+        def dfs(i):
+            if i in safe:
+                return safe[i]
+            safe[i] = False
+
+            for neg in graph[i]:
+                if not dfs(neg):
+                    return False
+            safe[i] = True
+            return safe[i]
+
+
+
+
+        res = []
+        for i in range(l):
+            if dfs(i):
+                res.append(i)
+        return res
