f7007289779de93dc2305d72d1de88e847d7075b
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-02-10 04:44:10 -0300

    Time: 13 ms (49.54%), Space: 20.9 MB (14.44%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        dic = defaultdict(list)
+        r , c = len(mat),len(mat[0])
+        ans = []
+        for i in range(r):
+            for j in range(c):
+                dic[i+j].append(mat[i][j])
+        for i in range(len(dic)):
+            if i % 2 == 0:
+                dic[i].reverse()
+                ans += dic[i]
+            else:
+                ans += dic[i]
+        return ans
+
+        
