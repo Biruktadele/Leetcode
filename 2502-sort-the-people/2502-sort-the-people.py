9e0ddbc9ce51e9c774a85d778a14107379aa34b0
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-02-10 05:10:47 -0300

    Time: 919 ms (5.03%), Space: 18 MB (82.01%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -4,10 +4,16 @@ class Solution:
         for i in range(ln):
             l = 0
             r = 1
+            flag = True
             while r < ln:
+                
                 if heights[l] < heights[r]:
                     heights[l] , heights[r] =  heights[r] , heights[l]
                     names[l], names[r] = names[r], names[l]
+                    flag = False
+            
                 r += 1
                 l += 1
+            if flag:
+                break
         return names
