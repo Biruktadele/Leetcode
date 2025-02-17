c5a45197b959cf20b3509fbd3c30689c70c62d0e
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-02-17 10:34:46 -0300

    Time: 88 ms (12.32%), Space: 41.2 MB (96.56%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.py
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -1,19 +1,22 @@
 class Solution:
-    def shiftingLetters(self, s: str, shifts: list[list[int]]) -> str:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         n = len(s)
-        shift = [0] * (n + 1)
-
-        for shiftOp in shifts:
-            start, end, direction = shiftOp
-            shift[start] += (1 if direction == 1 else -1)
-            if end + 1 < n:
-                shift[end + 1] -= (1 if direction == 1 else -1)
-
-        currentShift = 0
-        shiftList = list(s)
-        for i in range(n):
-            currentShift += shift[i]
-            netShift = (currentShift % 26 + 26) % 26
-            shiftList[i] = chr((ord(shiftList[i]) - ord('a') + netShift) % 26 + ord('a'))
-
-        return ''.join(shiftList)
+        arr = [0] *(n+1)
+        for i in shifts:
+            if i[2] == 1: 
+                arr[i[0]] += 1
+                arr[i[1]+1] -= 1
+            else:
+                arr[i[0]] -= 1
+                arr[i[1]+1] += 1
+        print(arr)
+        for i in range(1, n):
+            arr[i] += arr[i-1]
+        st = []
+        print(arr)
+        for j , i in enumerate(s):
+            n = arr[j]%26
+            new_word = chr(((ord(i)-ord('a'))%26 + n)%26+ord('a') )
+            st.append(new_word)
+        return "".join(st)
+    
