33605e7006d344924bb0c258eaa771deb0db6d7e
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-02-17 03:25:45 -0300

    Time: 4176 ms (5.01%), Space: 572 MB (6.91%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+    
+        ans = []
+        def dfs(s ,c , vist ,path, f):
+            if c == len(s):
+                sr = "".join(path)
+                ans.append(sr)
+                return
+            for i in range(0, len(s)):
+                if i in vist:
+                    continue
+                vist.add(i)
+                path.append(s[i])
+                dfs(s, c+1, vist, path , i+1)
+                path.pop()
+                dfs(s, c+1, vist, path , i+1)
+                vist.remove(i)
+                
+
+        dfs(tiles, 0 , set() ,[], 0)
+        return (len(set((ans))) - 1)
