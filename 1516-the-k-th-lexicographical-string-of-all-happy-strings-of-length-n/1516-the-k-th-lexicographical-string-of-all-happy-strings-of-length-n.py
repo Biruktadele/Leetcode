63b16a93a236b6a1940c0a7bb9b155d3e384defb
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-02-19 03:29:19 -0300

    Time: 1 ms (86%), Space: 17.9 MB (66.08%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -0,0 +1,24 @@
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        s = "abc"
+
+        self.count = 0
+        ans = []
+        def dfs(s,n,k,path):
+           
+            if len(path) == n:
+                self.count += 1
+                if self.count == k:
+                    ans.append("".join(path[:]))
+                return 
+            for i in range(3):
+                if not path or path[-1] != s[i]:
+                    path.append(s[i])
+                    dfs(s,n,k,path)
+                    path.pop()
+                    if len(ans) > 0:
+                        return
+
+            
+        dfs(s,n,k,[])
+        return ans[0] if ans else ""
