1734dd8d9f85d4f2630eb0ae631c385f249707f4
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-03-03 12:14:17 -0300

    Time: 3 ms (78.48%), Space: 21.8 MB (95.3%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        b5 = 0
+        b10 = 0
+
+        for i in bills:
+            if i == 5:
+                b5 += 5
+            elif i == 10:
+                if b5:
+                    b5 -= 5
+                    b10 += 10
+                else:
+                    return False
+            else:
+                if b10 and b5:
+                    b10 -= 10
+                    b5 -= 5
+                elif b5 >= 15:
+                    b5 -= 15
+                else:
+                    return False
+        return True
+
+
+    
+        
+        
