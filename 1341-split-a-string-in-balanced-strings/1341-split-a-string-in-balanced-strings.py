34e07db49c35a09f6c1c292bc7ae7636b1175a8a
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-03-04 03:58:35 -0300

    Time: 0 ms (100%), Space: 18 MB (8.48%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        c = 0
+        n = 0
+        for i in s:
+            if i == "L":
+                n -= 1
+            else:
+                n += 1
+            if n == 0:
+                c += 1
+        return c
+        
