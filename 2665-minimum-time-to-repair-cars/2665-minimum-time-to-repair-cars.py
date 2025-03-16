2a54858c4866af4121c28cd7142ae1d213169c8a
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-03-16 07:54:39 -0300

    Time: 1060 ms (30.41%), Space: 21.9 MB (84.79%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -1,20 +1,11 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        def is_can_repair(ranks , minute):
-            cars = 0
+        def is_can_repair( minute):
+            car = 0
             for i in ranks:
-                cars += int(sqrt(minute // i))
-            return cars
-
-        l = 0 
+                car += int(sqrt(minute // i))
+            return cars <= car
         r = max(ranks) * cars**2
-        
-        while r > l:
-            mid = (l+r)// 2
-     
-            if is_can_repair(ranks , mid) >= cars:
-                r = mid
-            else:
-                l = mid+1
-        return l
+    
+        return bisect_left(range(r), True, key=is_can_repair)
             
