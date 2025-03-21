8cdf76cfead3407cc3490da44386f02c3bbc8e63
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-03-21 13:05:18 -0300

    Time: 1988 ms (5.07%), Space: 17.9 MB (18.98%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.py
@@ -1,24 +1,46 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        stuck = [1]
-        i = 0
-        mx = 1
-        while i < len(pattern):
-            if pattern[i] == "I":
-                stuck.append(mx + 1)
-                mx =  mx + 1
-            else:
-                mn = stuck[-1]
-                j = len(stuck) -1
-                while mx > stuck[j]:
-                    stuck[j] += 1
-                    j -= 1
-                mx += 1
-                stuck[j] = mx
-                
-                stuck.append(mn)
-            
-            i += 1
-        for i in range(len(stuck)):
-            stuck[i] = str(stuck[i])
-        return "".join(stuck)
+
+        def check(s , num):
+            for i in range(1 ,len(num)):
+                if (s[i-1] == "I" and num[i] < num[i-1] ) or (s[i-1] == "D" and num[i] > num[i-1]):
+                    return False
+            return True
+        
+        s = "IIIDIDDD"
+        num = [1,2,3,5,4,9,8,7,6]
+        print(check(s , num))
+                    
+        ans = []
+        def permu(n  , path , p ,vist):
+            if len(path) == n:
+                if check(p , path):
+                    ans.append("".join(path))
+                return
+            if ans:
+                return
+            for i in range(1,n+1):
+                if i in vist:
+                    continue
+                path.append(str(i))
+                vist.add(i)
+                permu(n , path , p ,vist)
+                if ans:
+                    return
+                path.pop()
+                vist.remove(i)
+        permu(len(pattern) + 1  , [] , pattern ,set())
+       
+        return ans[0]
+
+
+
+
+
+
+
+
+
+
+
+
