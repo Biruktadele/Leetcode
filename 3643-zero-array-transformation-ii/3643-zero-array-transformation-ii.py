fac51037371d9d547a90c6f00d74c575e655c188
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-03-26 17:43:01 +0300

    Time: 1524 ms (5.44%), Space: 63.8 MB (19.62%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
@@ -1,32 +1,38 @@
+from typing import List
+
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-        n = len(nums)
-        summ = 0
-        pre = [0] * (n+1)
-        left = 0
-        for i in range(len(queries) + 1):
-            while left < n and  pre[left] + summ >= nums[left]:
-                summ += pre[left]
-                left += 1
-            if left == n:
-                return i
-            if i == len(queries):
-                return -1
-            l,r,val = queries[i]
-            if l > left:
-                pre[l] += val
-                pre[r+1] -= val
-            elif r >= left:
-                summ += val
-                pre[r+1] -= val
 
+        def zero(arr , q , lim):
+            pre = [0]*(len(arr) +1)
+            if lim > len(q):
+                return False
+            for i in range(lim):
+                pre[q[i][0]] += q[i][2]
+                pre[q[i][1] + 1] -= q[i][2]
+            for i in range(1,len(pre)):
+                pre[i] += pre[i-1]
+            pre.pop()
 
+            for i , j in zip(arr , pre):
+                if i > j:
+                    return False
+            return True
 
-
-            
-
-
-
-
-
+        l = 0
+        r = len(queries)
+        best = -1
         
+        while l <= r:
+            mid = l + (r - l) // 2
+
+            if zero(nums , queries , mid):
+                r = mid - 1
+                best = mid
+            else:
+                l = mid + 1
+        return best
+
+
+
+       
