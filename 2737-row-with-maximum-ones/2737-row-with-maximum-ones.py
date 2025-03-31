d3459e1fe427a3ef2d29e9cdbfb75392397df133
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-03-31 14:53:16 +0300

    Time: 5 ms (83.13%), Space: 18.4 MB (63.5%) - LeetHub

---
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
+        maxx = 0
+        ind = 0
+        for j , i in enumerate(mat):
+            c = i.count(1)
+            if c > maxx:
+                maxx = c
+                ind = j
+        return [ind , maxx]
