d4b628d9ab06ed60805018203fcb0524cbaf2422
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-04-17 13:37:57 -0300

    Time: 298 ms (19.6%), Space: 19.5 MB (29.39%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,63 +1,26 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        q = deque()
-        if grid[0][0] != 0:
+    
+        DIRECTIONS_8 = [(-1, -1), (-1, 1), (1, -1), (1, 1), (-1, 0), (1, 0), (0, -1), (0, 1)]
+        def inbound(x, y, n, m): return 0 <= x < n and 0 <= y < m
+        if grid[0][0] == 1:
             return -1
-        q.append((0,0))
-        vist =set()
-        vist.add((0,0))
-        row ,col = len(grid)-1 , len(grid[0])-1
-        if row == 0 and col == 0:
-            return 1
-        count = 1
-
+        q = deque([(0,0)])
+        vist = set((0,0))
+        row , col = len(grid) , len(grid[0])
+        level = 1
         while q:
-            way = False
             l = len(q)
-            
             for i in range(l):
-                r , c = q.popleft()
-                if r > 0 and grid[r-1][c] == 0 and (r-1,c) not in vist:
-                    vist.add((r-1,c))
-                    q.append((r-1,c))
-                    way = True
-                if r > 0 and c > 0 and grid[r-1][c-1] == 0 and (r-1,c-1) not in vist:
-                    vist.add((r-1,c-1))
-                    q.append((r-1,c-1))
-                    way = True
-                if c > 0 and grid[r][c-1] == 0 and (r,c-1) not in vist:
-                    vist.add((r,c-1))
-                    q.append((r,c-1))
-                    way = True
-                if r < row and c > 0 and grid[r+1][c-1] == 0 and (r+1,c-1) not in vist:
-                    vist.add((r+1,c-1))
-                    q.append((r+1,c-1))
-                    way = True
-                if r < row and grid[r+1][c] == 0 and (r+1,c) not in vist:
-                    vist.add((r+1,c))
-                    q.append((r+1,c))
-                    way = True
-                if r < row and c < col and grid[r+1][c+1] == 0 and (r+1,c+1) not in vist:
-                    vist.add((r+1,c+1))
-                    q.append((r+1,c+1))
-                    print("yes")
-                    way = True
-                if r > 0 and c < col and grid[r-1][c+1] == 0 and (r-1,c+1) not in vist:
-                    vist.add((r-1,c+1))
-                    q.append((r-1,c+1))
-                    way = True
-                if c < col and grid[r][c+1] == 0 and (r,c+1) not in vist:
-                    vist.add((r,c+1))
-                    q.append((r,c+1))
-                    way = True
-            count += 1
-            if (row ,col) in vist:
-                return count
-            if not way:
-                return -1    
-            
-        return count
-
-
-        
-        
+                curr = q.popleft()
+                if curr == (row-1, col-1):
+                    return level
+                for u , v in DIRECTIONS_8:
+                    nr = u + curr[0]
+                    nc = v + curr[1]
+                    if not inbound(nr , nc , row , col) or grid[nr][nc] == 1 or (nr , nc) in vist:
+                        continue
+                    q.append((nr,nc))
+                    vist.add((nr,nc))
+            level += 1
+        return -1
