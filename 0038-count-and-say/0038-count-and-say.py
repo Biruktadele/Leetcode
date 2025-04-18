6b021813d33aeef5e57e4c8d4bf1dec225ce1005
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-04-18 15:58:02 -0300

    Time: 3 ms (97.1%), Space: 18.2 MB (9.42%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,26 +1,25 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
         def sey(s):
-            d = {}
-            ch = s[0]
+            t = s[0]
             c = 1
-            st = ""
-            for i in range(1,len(s)):
-                if s[i] == ch:
-                    c+= 1
+            ans = []
+            for i in s[1::]:
+                if i == t:
+                    c += 1
                 else:
-                    st += str(c)+ch
-                    ch = s[i]
+                    ans.append(str(c))
+                    ans.append(t)
+                    t = i
                     c = 1
-            st += str(c)+ch
-            return st
-        i = 1
-        s = '1'
-        if n == 1:
-            return '1'
-        while i < n:
-            s = sey(s)
-            i += 1
-        return s
+            ans.append(str(c))
+            ans.append(t)
+            return "".join(ans)
+        def count(n):
+            if n == 1:
+                return "1"
+            return sey(count(n-1))
+        return count(n)
             
+
         
