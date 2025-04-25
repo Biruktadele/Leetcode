4911ca6ec10281d85e0ff0ec09d7aa0d4663ed68
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-04-25 16:43:57 -0300

    Time: 7 ms (28.15%), Space: 19 MB (72.35%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
         ord = []
         graph = defaultdict(list)
         indegree = [0]*  numCourses
