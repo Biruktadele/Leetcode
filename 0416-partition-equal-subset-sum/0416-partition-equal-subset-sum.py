5cb36e178d4584a8762debb3b55389977d360e0d
Biruk Tadele <106767562+Biruktadele@users.noreply.github.com>
2025-10-27 18:01:14 +0300

    Time: 0 ms (100%), Space: 17.9 MB (81.27%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,13 +1,8 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        sum1=sum(nums)
-        if sum1&1: return False
-        K=sum1//2
+    def canPartition(self, nums: list[int]) -> bool:
 
-        @cache 
-        def f(i, sum1):
-            if sum1==K: return True
-            if i<0 or sum1>K: return False
-            return f(i-1, sum1+nums[i]) or f(i-1, sum1)
-            
-        return f(len(nums)-1, 0)
+        target, isOdd = divmod(sum(nums), 2)
+        if isOdd: return False
+
+        bitmap = reduce(lambda x, y: x|(x << y), nums, 1)
+        return bitmap & (1 << target) != 0
